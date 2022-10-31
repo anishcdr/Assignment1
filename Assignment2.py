251ec0d3ab09bc957d694c99225df9185732454b
anishcdr <112460231+anishcdr@users.noreply.github.com>
2022-10-31 19:59:59 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -20,3 +20,14 @@ else:
   print("Not a valid operator")
   file1.write("Not a valid operator")
 print(file1.read())
+
+#2.
+a=open("a2.txt","r+")
+a.write("Writing with r+")
+print(a.read())
+a=open("a2.txt","w+")
+a.write("Overwriting with w+\n")
+print(a.read())
+a=open("a2.txt","a+")
+a.write("Writing with a+\n")
+print(a.read())
